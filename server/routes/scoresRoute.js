58d0685c6ca0c3418b2438b1592e242b7afcd25d
Vance Wallace <vancito@gmail.com>
2019-08-03 12:07:48 -0700

    changed test responses to a single line

--- server/routes/scoresRoute.js
+++ server/routes/scoresRoute.js
@@ -3,8 +3,7 @@ const scoresRouter = require('express').Router();
 // controllers
 const scoresController = require('../controllers/scoresController.js');
 scoresRouter.post('/', (req,res) => {
-    res.status(200)
-    res.send('Hello, you have reached the scores route.')
+    res.status(200).send('Hello, you have reached the scores route.')
     }
 )
 // authRouter.post('/signup', authController.createUser);
--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -3,8 +3,7 @@ const usersRouter = require('express').Router();
 // controllers
 const usersController = require('../controllers/usersController.js');
 usersRouter.get('/', (req,res) => {
-    res.status(200)
-    res.send('Hello, you have reached the users route.')
+    res.status(200).send('Hello, you have reached the users route.')
     }
 ) 
 // authRouter.post('/signup', authController.createUser);
