f022d40e413273d50c176512e94affe99ab18098
Devon <devonev92@gmail.com>
2019-08-02 21:22:30 -0700

    added material-ui and theme provider

--- client/App.js
+++ client/App.js
@@ -1,4 +1,20 @@
 import React, {Component} from 'react';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import {Button} from '@material-ui/core';
+
+let theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+theme = responsiveFontSizes(theme);
 
 class App extends Component {
 constructor() {
@@ -9,9 +25,12 @@ constructor() {
 
   render() { 
     return (
-      <div>
-        Hello World
-      </div>
+      <ThemeProvider theme = {theme}>
+        <Button color = "secondary">
+          Hello World
+        </Button>
+      </ThemeProvider>
+      
     );
   }
 }
