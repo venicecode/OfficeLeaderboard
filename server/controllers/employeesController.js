d0c4142cbda80ceb2554784301cb8a26eee2bd1b
Vance Wallace <vancito@gmail.com>
2019-08-03 18:20:25 -0700

    this one too

--- server/controllers/employeesController.js
+++ server/controllers/employeesController.js
@@ -1,9 +1,10 @@
 const employeesController = {};
+
 employeesController.getInfo = (req,res,next) => {
     const userSearchTerm = req.params.userid;
     console.log("Here is the userID we are looking for in the DB: ",userSearchTerm);
     //search the DB for the userID (an asynchronus request)
-    //.then return the matching avatar, games and username 
+    //.then
         //if we can't find the userID throw an error
         //else return the info in the response body
     res.send({avatar: "avatar", games: "games", username: "name"})
