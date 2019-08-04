1aa28765c53b80ac2cacc2e0a3f7ee89dbbc25a0
Vance Wallace <vancito@gmail.com>
2019-08-03 19:34:01 -0700

    4/6 fake things working

--- server/controllers/statsController.js
+++ server/controllers/statsController.js
@@ -1,4 +1,5 @@
 const statsController = {};
+
 statsController.getLeaderBoard = (req,res,next) => {
     const officeHostingGame = req.params.office;
     const gameAtThisOffice = req.params.game;
@@ -10,13 +11,14 @@ statsController.getLeaderBoard = (req,res,next) => {
         //else return the info in the response body 
     res.send({Username1: "Vance", Rank1: "1", Username2: "Alex", Rank2: "2", Username3: "Tang", Rank3: "3"})
 }
+
 statsController.moveUser = (req,res,next) => {
     const gameToChange = req.params.game;
     const userToMove = req.params.userid;
-    // const rankDirection = req.body.rank;
+    const rankDirection = req.body.rank;
     console.log("Changing the following game: ", gameToChange);
     console.log("We are going to rank this user: ", userToMove);
-    // console.log("In this way: ", rankDirection);
+    console.log("In this way: ", rankDirection);
     //not sure how we will do this........
     res.send({NewLeaderBoard: "someLeaderBoardArr"}) 
 };
